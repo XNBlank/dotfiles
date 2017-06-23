7c07ecf2dbfadcd22de27e3b354750fa5b8fa304
XNBlank <koruten@gmail.com>
2017-06-23 17:22:06 -0400

    -m

--- home/.config/polybar/updates.sh
+++ home/.config/polybar/updates.sh
@@ -1,1 +1,13 @@
-/home/blank/.homesick/repos/dotfiles/home/.config/polybar/updates.sh
+#!/bin/bash
+pac=$(checkupdates | wc -l)
+aur=$(cower -u | wc -l)
+
+check=$((pac + aur))
+
+if [[ $check < 26 ]]
+then
+    echo "$pac %{F#5b5b5b}%{F-} $aur"
+elif [[ $check > 25 ]]
+then
+    echo "$pac %{F#cc5b5b}%{F-} $aur"
+fi
