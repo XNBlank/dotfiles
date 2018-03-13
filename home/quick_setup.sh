c0a0c1444b07380ee9de41c316ac3396ae7a0dae
Colton Wiens <XNBlank@users.noreply.github.com>
2018-03-13 11:40:02 -0700

    quick_setup.sh
    shell script to install everything I need from a new install.

---
+++ home/quick_setup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Make sure our shiny new arch is up-to-date
+echo "Checking for system updates..."
+sudo pacman -Syu
+
+### Installing Pacaur
+# Create a tmp-working-dir and navigate into it
+mkdir -p /tmp/pacaur_install
+cd /tmp/pacaur_install
+
+# Install pacaur dependencies from arch repos
+sudo pacman -S expac yajl git --noconfirm --needed
+
+# Install cower from AUR
+if [ ! -n "$(pacman -Qs cower)" ]; then
+    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+    makepkg PKGBUILD --skippgpcheck --install --needed
+fi
+
+# Install pacaur from AUR
+if [ ! -n "$(pacman -Qs pacaur)" ]; then
+    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+    makepkg PKGBUILD --install --needed
+fi
+
+# Clean up
+cd ~
+rm -r /tmp/pacaur_install
+
+### Installing my stuff
+
+# Install xorg
+if [ ! -n "$(pacman -Qs xorg-server)" ]; then
+    sudo pacman -S xorg-server xorg-xclock xorg-xinit xterm thunar sddm --noconfirm --needed
+fi
+
+# Install WM and stuff
+pacaur -S i3-gaps-next-git i3-status i3lock-git otf-font-awesome-4 polybar-git rofi-git dunst-git termite-git compton-git --noconfirm --noedit --needed
+
+# Install personal apps
+pacaur -S discord-canary firefox-nightly neovim-git spotify mpv-git youtube-dl-git --noconfirm --noedit --needed
+
+sudo systemctl set-default graphical.target 
+sudo systemctl enable sddm.service
+sudo systemctl start sddm.service 
